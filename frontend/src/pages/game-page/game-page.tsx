e5ccc6a75e7cbfa752418a110ff3c1d497d22661
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-05-20 03:28:26 +0300

    FE: Separate countdown modes. Improve in-game controls and stats

--- frontend/src/pages/game-page/game-page.tsx
+++ frontend/src/pages/game-page/game-page.tsx
@@ -3,26 +3,42 @@ import { useEffect, useState } from "react";
 import calculatePoints from "../../helpers/calculate-points";
 import type { Click } from "../../types/click";
 import { GameState, GameStateName, type Game } from "../../types/game";
-import { getGameEndTimestamp, getGameState } from "../../data/initial-games";
+import { getGameCooldownTimestamp, getGameEndTimestamp, getGameState } from "../../data/initial-games";
 
 export default function GamePage() {
   const game = useLoaderData<Game>();
 
   const [clicks, setClicks] = useState<Click[]>([]);
   const [timeLeft, setTimeLeft] = useState(-1);
-  const [timer, setTimer] = useState<number|undefined>(undefined);
+  const [timer, setTimer] = useState<number | undefined>(undefined);
   const [gameState, setGameState] = useState(getGameState(game));
 
-  useEffect(function() {
-    setTimer(setInterval(function() {
-      setGameState(getGameState(game));
-      setTimeLeft(getGameEndTimestamp(game) - Date.now());
-    }, 1000));
+  function cleanup() {
+    clearInterval(timer);
+    setTimer(undefined);
+    setGameState(getGameState(game));
+  }
 
-    return function() {
-      clearInterval(timer);
-      setTimer(undefined);
+  function updateTimestamp() {
+    const currentGameState = getGameState(game);
+
+    if (currentGameState === GameState.ENDED) {
+      cleanup();
+      return;
     }
+
+    const nextTimestamp = [GameState.WAITING, GameState.COOLDOWN].includes(currentGameState)
+      ? getGameCooldownTimestamp(game)
+      : getGameEndTimestamp(game);
+
+    setGameState(currentGameState);
+    setTimeLeft(nextTimestamp - Date.now());
+  }
+
+  useEffect(function () {
+    setTimer(setInterval(updateTimestamp, 1000));
+    updateTimestamp();
+    return cleanup;
   }, []);
 
   function handleDuckClick() {
@@ -44,12 +60,20 @@ export default function GamePage() {
       <div className="game-header-username">%username%</div>
     </header>
     <main className="game-round">
-      <button onClick={handleDuckClick} className="game-round-clicker" style={{
-        zoom: 3,
-      }}>Click me!</button>
+      <button
+        disabled={[GameState.WAITING, GameState.COOLDOWN, GameState.ENDED].includes(gameState)}
+        onClick={handleDuckClick}
+        className="game-round-clicker"
+        style={{
+          zoom: 3,
+        }}
+      >Click me!</button>
       <div className="game-round-stats">
         <div className="game-round-stats-state">{GameStateName.get(gameState)}</div>
-        <div className="game-round-stats-timer">{timeLeft}</div>
+        <div className="game-round-stats-timer" hidden={[GameState.ENDED].includes(gameState)}>{
+          (GameState.ACTIVE === gameState ? "До конца раунда " : null) ||
+          ("До начала раунда")
+        }{timeLeft}</div>
         <div className="game-round-stats-points">{calculatePoints(clicks)}</div>
       </div>
     </main>
