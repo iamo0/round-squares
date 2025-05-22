2596d67a06d99e66038d6768182b23954a7ed397
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-05-22 07:03:17 +0300

    BE: Add migrations for creation of games list

---
+++ backend/migrations/20250522035832-create-game.js
@@ -0,0 +1,31 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Games', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.UUID
+      },
+      start: {
+        type: Sequelize.TIME
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Games');
+  }
+};
