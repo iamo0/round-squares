ea7473dd008247437b93e7456632a4dd40d952d4
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-05-23 11:00:45 +0300

    BE: Introduce clicks into game data. Add handle for saving clicks

--- backend/src/routers/games-router.ts
+++ backend/src/routers/games-router.ts
@@ -1,10 +1,18 @@
 import { Request, Response, Router } from "express";
 import getRandom from "../helpers/get-random";
 
-function getNewGame(i: number, start: number) {
+function getNewGame(i: number, start: number):{
+  id: string,
+  start: Date,
+  clicks: {
+    user?: string,
+    date: Date
+  }[],
+} {
   return {
     id: (i + 1).toString(),
     start: new Date(start),
+    clicks: [],
   };
 }
 
@@ -17,19 +25,26 @@ const games = new Array(5)
     return getNewGame(i, start);
   });
 
-
 export default function initializeGameRouter(_sequelize: any) {
   const gamesRouter = Router();
 
   gamesRouter.get("/", async function (req: Request, res: Response) {
-    res.status(200).json(games.sort((a, b) => +b.start - +a.start));
+    res
+      .status(200)
+      .json(
+        games
+          .map(({ id, start }) => ({ id, start }))
+          .sort((a, b) => +b.start - +a.start)
+      );
   });
 
   gamesRouter.post("/", async function (req: Request, res: Response) {
     const newGameStart = req.body.date;
     const newGame = getNewGame(games.length, newGameStart);
     games.push(newGame);
-    res.status(201).json(newGame);
+
+    const { id, start } = newGame;
+    res.status(201).json({ id, start });
   });
 
   gamesRouter.get("/:gameId", async function (req: Request, res: Response) {
@@ -45,7 +60,34 @@ export default function initializeGameRouter(_sequelize: any) {
       return;
     }
 
-    res.status(200).json(game);
+    const { id, start } = game;
+    res.status(200).json({ id, start });
+  });
+
+  gamesRouter.post("/:gameId", async function (req: Request, res: Response) {
+    const { gameId } = req.params;
+    const game = games.find((g) => g.id === gameId);
+
+    if (game === undefined) {
+      res.status(404).json({
+        errors: [{
+          message: `Can't find a game with id ${req.params.gameId}`,
+        }],
+      });
+      return;
+    }
+
+    const { clicks }: {
+      clicks: { date: string }[]
+    } = req.body;
+
+    game.clicks = [...game.clicks, ...clicks.map(({ date }) => ({
+      date: new Date(date),
+    }))];
+
+    res
+      .status(200)
+      .json(game.clicks);
   });
 
   return gamesRouter;
